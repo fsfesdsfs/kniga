ed91ac77a1401fa5e8d0db7d3469cca0f80a6d66
fsfesdsfs <68592763+fsfesdsfs@users.noreply.github.com>
2026-02-18 04:05:47 -0800

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ if st.button("Check Book"):
     st.warning("Please enter a book")
 elif user_input in books:
   st.succes("The book you entered does exist in the database")
-else
+else:
   st.error("The book you entered is invalid and/or doesnt exist in our database")
 
          
