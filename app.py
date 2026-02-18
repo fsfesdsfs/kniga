d8045831f9ec572d2840e316bb2d897a8c4fb277
fsfesdsfs <68592763+fsfesdsfs@users.noreply.github.com>
2026-02-18 03:57:59 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
+import atreamlit as st
+books = [
+  "The hobit"
+  "1983"
+  "The Great Gatsby"
+  "To kill a Mockingbirf"
+  "PRide and predjuce:
+]
+st.title("Book Checkmark")
+st.write("Enter a book to check if it exists in the database")
+user_input = st.text_input("Book Title")
+if st.button("Check Book"):
+if user_input.strip() == "":
+st.warning("Please enter a book")
+elif user_input in books:
+st.succes("The book you entered does exist in the database")
+else
+st.error("The book you entered is invalid and/or doesnt exist in our database")
 
+         
